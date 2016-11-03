0927c6afef8e93ade557ab779fd5c129a84d7a3e
faraz2550 <faraz_2550@yahoo.com>
2016-11-03 11:59:24 +0330

    Update scraping wb.py

--- scraping wb.py
+++ scraping wb.py
@@ -6,7 +6,7 @@ Created on Fri Sep 23 08:26:51 2016
 """
 
 import os
-os.chdir('D:\Coursera\Data Wrangling with MongoDB\practice\All imdb pages')
+os.chdir('D:\DataScience\Data Wrangling with MongoDB\practice\All imdb pages')
 import urllib
 import urllib2
 import time
@@ -88,8 +88,6 @@ dataf.to_csv('all_nominated.csv',index=False)
 
 ##      now im gonna download all the pages, 1.35 GB !!! :(
 
-os.chdir('D:\Coursera\Data Wrangling with MongoDB\practice\All imdb pages')
-
 data=[]
 with open('all_nominated.csv','r') as f:
     reader=csv.reader(f)
@@ -117,53 +115,6 @@ def get_pages(p1,p2):
 
 get_pages(4522,4688)
 
-
-############   mining the html's
-
-data=[]
-with open('all_nominated.csv','r') as f:
-        the_list=[]
-        for i in csv.reader(f):
-            the_list.append(i)  
-the_list=the_list[1:]
-data=pd.DataFrame()
-
-def web_scraper(p1,p2):
-    for i in range(p1,p2):
-        page_num=i
-        print 'Scraping Page ' + str(page_num) 
-        search_page='%s-%s.html' %(i,the_list[i][0].replace(':','').replace('?','').replace('/',''))
-        data[i][0]=the_list[i][1]
-        data[i][1]=the_list[i][0].replace(':','').replace('?','').replace('/','')
-        with open(search_page,'r') as page:
-            soup=BeautifulSoup(page,'lxml')
-            for q in soup.find(itemprop='ratingValue'):
-                data[i][2]=q
-            for q in soup.find(itemprop='ratingCount'):
-                data[i][3]=q.replace(',','')
-            
-                for j in q.find_all('a'):
-                    if len(j.get('href'))==32:
-                        a=[0,0]
-                        a[0]= j.text
-                        a[1]= j.get('href').split('?')[0]
-                        data.append(a)
-            print 'Page %d is Done \n' %page_num
-            check=False
-    dataf.to_csv('all_nominated.csv',index=False)
-    return
-
-get_titles_links(1,95)
-
-
-web_scraper(0,4)
-search_page='1-The Revenant.html'
-search_page='%s-%s.html' %(i,the_list[i][0].replace(':','').replace('?','').replace('/',''))
-        with open(search_page,'r') as page:
-            soup=BeautifulSoup(open(search_page),'lxml')
-            print soup.h1
-            for q in soup.find(itemprop="ratingCount"):
-                print q.replace(',','')
 
 
 
